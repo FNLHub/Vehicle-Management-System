9657ada6628e1850e2fb6c4d6c8286af0d36029e
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2017-05-08 16:41:02 -0700

    Frontend changes
    Hooked up the vehicle and card options to go to the same page, but with
    differences

--- Facilities - dead as of 5-7-17/FacilitiesCheckOut/Controllers/HomeController.cs
+++ Facilities - dead as of 5-7-17/FacilitiesCheckOut/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
 
 namespace FacilitiesCheckOut.Controllers
 {
@@ -23,45 +24,49 @@ namespace FacilitiesCheckOut.Controllers
         public ActionResult VehicleInOutChoose()//-vvvvvv-Vehicle Pages-vvvvvv-\\
         {
             ViewBag.Message = "Your application Vehicle Sign In/Out page.";
+            ViewBag.Title = "Checkout or Return Vehicle";
+            ViewBag.Choice = "Vehicle";
 
-            return View();
-        }
-
-        public ActionResult VehicleCheckout()
-        {
-            ViewBag.Message = "Your Vehicle Checkout page.";
-
-            return View();
-        }
-
-        public ActionResult VehicleReturn()
-        {
-            ViewBag.Message = "Your Vehicle Return Page.";
-
-            return View();
+            return View("CheckoutReturnChoose");
         }
 
         public ActionResult GasCardInOutChoose()//-vvvvvv-Gas Card Pages-vvvvvv-\\
         {
             ViewBag.Message = "Your application Gas Card Sign In/Out page.";
+            ViewBag.Title = "Checkout or Return Gas Card";
+            ViewBag.Choice = "Gas Card";
 
-            return View();
+            return View("CheckoutReturnChoose");
         }
 
-        public ActionResult GasCardCheckout()
+        public ActionResult Checkout()
         {
-            ViewBag.Message = "Your Gas Card Checkout Page.";
+            switch ((string)Session["type"])
+            {
+                case "Vehicle":
+                    ViewBag.Message = "Checkout a Vehicle";
+                    break;
+                case "Gas Card":
+                    ViewBag.Message = "Checkout a Gas Card";
+                    break;
+            }
 
-            return View();
+            return View("Reports");
         }
 
-        public ActionResult GasCardReturn()
+        public ActionResult Return()
         {
-            ViewBag.Message = "Your Gas Card Return Page.";
+            switch ((string)Session["type"])
+            {
+                case "Vehicle":
+                    ViewBag.Message = "Return a Vehicle";
+                    break;
+                case "Gas Card":
+                    ViewBag.Message = "Return a Gas Card";
+                    break;
+            }
 
-            return View();
+            return View("Reports");
         }
-
-
     }
 }
