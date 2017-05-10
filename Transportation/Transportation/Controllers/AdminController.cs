d64b1a98935236710d74bc10e2252e18d314a54a
JumperGerald <geraldj@cos.edu>
2017-05-10 13:57:41 -0700

    From Scratch (Sorta)
    Created a new project that links to my Local DB we can swap it over to
    the server friday

---
+++ Transportation/Transportation/Controllers/AdminController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Transportation.Controllers
+{
+    public class AdminController : Controller
+    {
+        TransportationEntities transportationContext = new TransportationEntities();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Create Statements for Users Table
+        [HttpGet]
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                transportationContext.Users.Add(user);
+                transportationContext.SaveChanges();
+                return RedirectToAction("index");
+            }
+            return View();
+        }
+
+        // Create Statements for Departments Table
+        [HttpGet]
+        public ActionResult CreateDepartment()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateDepartment(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                transportationContext.Departments.Add(department);
+                transportationContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // Create Statements for Vehicle Table
+        [HttpGet]
+        public ActionResult CreateVehicle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateVehicle(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                transportationContext.Vehicles.Add(vehicle);
+                transportationContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // Create Statements for Campus Table
+        [HttpGet]
+        public ActionResult CreateCampus()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCampus(Campus campus)
+        {
+            if (ModelState.IsValid)
+            {
+                transportationContext.Campuses.Add(campus);
+                transportationContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // Create Statements for VehicleStatus Table
+        [HttpGet]
+        public ActionResult CreateVehicleStatus()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateVehicleStatus(VehicleStatus vehicleStatus)
+        {
+            if (ModelState.IsValid)
+            {
+                transportationContext.VehicleStatuses.Add(vehicleStatus);
+                transportationContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // Create Statements for Keys Table
+        [HttpGet]
+        public ActionResult CreateKey()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateKey(Key key)
+        {
+            if (ModelState.IsValid)
+            {
+                transportationContext.Keys.Add(key);
+                transportationContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // Create Statements for GasCards Table
+        [HttpGet]
+        public ActionResult CreateGasCard()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateGasCard(GasCard gasCard)
+        {
+            if (ModelState.IsValid)
+            {
+                transportationContext.GasCards.Add(gasCard);
+                transportationContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // Create Statements for GasCardStatuses Table
+        [HttpGet]
+        public ActionResult CreateGasCardStatus()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateGasCardStatus(GasCardStatus gasCardStatus)
+        {
+            if (ModelState.IsValid)
+            {
+                transportationContext.GasCardStatuses.Add(gasCardStatus);
+                transportationContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Transportation/Transportation/Controllers/TransportationController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Transportation.Controllers
+{
+    public class TransportationController : Controller
+    {
+        TransportationEntities transportationContext = new TransportationEntities();
+        
+        
+        [HttpGet] // GET: Transportation
+        public ActionResult Index()
+        {
+            return View(transportationContext.Transportation_View.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult SignOut()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignOut(SignOut trans)
+        {
+            if (ModelState.IsValid)
+            {
+                trans.CheckOutTime = DateTime.Now;
+                trans.ActivityTime = DateTime.Now;
+                transportationContext.SignOuts.Add(trans);
+                transportationContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SignIn(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(Response.StatusCode = 400);
+            }
+            SignOut trans = transportationContext.SignOuts.Find(id);
+            if (trans == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trans);
+        }
+        [HttpPost]
+        public ActionResult SignIn(SignOut trans)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    trans.CheckOutTime = trans.CheckOutTime;
+                    trans.CheckInTime = DateTime.Now;
+                    trans.ActivityTime = DateTime.Now;
+                    transportationContext.Entry(trans).State = System.Data.Entity.EntityState.Modified;
+                    transportationContext.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(trans);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(Response.StatusCode = 400);
+            }
+            Transportation_View trans = transportationContext.Transportation_View.Find(id);
+            if (trans == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trans);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(Response.StatusCode = 400);
+            }
+            Transportation_View trans = transportationContext.Transportation_View.Find(id);
+            if (trans == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trans);
+        }
+        [HttpPost]
+        public ActionResult Delete(int? id, Transportation_View transport)
+        {
+            try
+            {
+                Transportation_View trans = new Transportation_View();
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                    {
+                        return new HttpStatusCodeResult(Response.StatusCode = 400);
+                    }
+                    trans = transportationContext.Transportation_View.Find(id);
+                    if (trans == null)
+                    {
+                        return HttpNotFound();
+                    }
+                    transportationContext.Transportation_View.Remove(trans);
+                    transportationContext.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(trans);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+    }
+}
