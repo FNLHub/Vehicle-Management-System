1e481ca34e6c142ae92573e274880bfb91f0c848
JumperGerald <geraldj@cos.edu>
2017-05-10 18:04:26 -0700

    Revert "Revert "Reports""
    This reverts commit 9b0ce3b83b2d2737c1f6763f99e0a4d094c554ba.

---
+++ Transportation/Transportation/Controllers/ReportsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Transportation.Controllers
+{
+    public class ReportsController : Controller
+    {
+        FacilitiesDBEntities transportationContext = new FacilitiesDBEntities();
+
+        // GET: Reports
+        public ActionResult Reports()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetUsers()
+        {
+            return View(transportationContext.Users.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult GetVehicles()
+        {
+            return View(transportationContext.Vehicles.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult GetGasCards()
+        {
+            return View(transportationContext.GasCards.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult GetSignedOut()
+        {
+            return View(transportationContext.Transportation_SignedOutView.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult GetSignOutLog()
+        {
+            return View(transportationContext.Transportation_ViewAll.ToList());
+        }
+    }
+}
