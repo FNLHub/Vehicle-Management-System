330e55d8893859636132775b471edc75b4523854
Cory Lewis <cjl9703@yahoo.com>
2017-05-17 22:55:01 -0700

    Edit Controller Update
    *Edit Gas Card views added into EditController
    *Edit Vehicle views added into EditController
    Note: unable to test due to no VPN access.

--- Transportation/Transportation/Controllers/EditController.cs
+++ Transportation/Transportation/Controllers/EditController.cs
@@ -10,6 +10,7 @@ namespace Transportation.Controllers
     {
         FacilitiesDBEntities transportationContext = new FacilitiesDBEntities();
 
+        //=========================================     Edit User     ===================================================
         [HttpGet]
         public ActionResult EditUser(int? id)
         {
@@ -39,7 +40,7 @@ namespace Transportation.Controllers
                         transportationContext.Entry(user).State = System.Data.Entity.EntityState.Modified;
                         transportationContext.SaveChanges();
                     }
-                
+
                     return RedirectToAction("Index", "Admin");
                 }
                 return View(user);
@@ -49,5 +50,97 @@ namespace Transportation.Controllers
                 return View();
             }
         }
+
+        //=========================================    Edit Gas Card    ===================================================
+        [HttpGet]
+        public ActionResult EditGasCard(int ? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(Response.StatusCode = 400);
+            }
+            var gasCard = transportationContext.GasCards.Find(id);
+            if (gasCard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gasCard);
+        }
+        [HttpPost]
+        public ActionResult EditGasCard(GasCard gasCard)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Select the gas card id which matches GasCardName with the selected gas card
+                    var gasCardId = transportationContext.GasCards.Where(G => G.GasCardName == gasCard.GasCardName).Select(i => i.GasCardId).FirstOrDefault();
+                   
+                    if (gasCardId != 0)
+                    {
+                        //Possible logic error on the two lines below
+                        gasCard.StatusId = gasCardId;
+                        gasCard.GasCardStatus.StatusId = gasCardId;
+                        transportationContext.Entry(gasCard).State = System.Data.Entity.EntityState.Modified;
+                        transportationContext.SaveChanges();
+                    }
+
+                    return RedirectToAction("Index", "Admin");
+                }
+                return View(gasCard);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //=========================================     Edit Vehicle     ===================================================
+        [HttpGet]
+        public ActionResult EditVehicle(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(Response.StatusCode = 400);
+            }
+            var vehicle = transportationContext.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+        [HttpPost]
+        public ActionResult EditVehicle(Vehicle vehicle)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Select the vehicle id where the names match
+                    var vehicleId = transportationContext.Vehicles.Where( v => v.VehicleName == vehicle.VehicleName).Select(i => i.VehicleId).FirstOrDefault();
+                 
+                    if (vehicleId != 0)
+                    {
+
+                        //possible logic error on the 2 lines below
+                        vehicle.VehicleId = vehicleId;
+                        vehicle.VehicleStatus.StatusId = vehicleId;
+                    
+                        //Saving modified entity
+                        transportationContext.Entry(vehicle).State = System.Data.Entity.EntityState.Modified;
+                        transportationContext.SaveChanges();
+                    }
+
+                    return RedirectToAction("Index", "Admin");
+                }
+                return View(vehicle);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
     }
 }
