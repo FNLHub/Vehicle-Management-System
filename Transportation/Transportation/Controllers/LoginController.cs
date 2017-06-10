5f054f289be288c2ee5108db6aef629b7c3cec27
Ahsen Baig <ahsenbaig@gmail.com>
2017-06-09 18:15:57 -0700

    Added the login controller and put the basic view in.

---
+++ Transportation/Transportation/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Transportation.Models;
+
+namespace Transportation.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            ViewBag.Title = "Login 2 Test";
+            var loginViewModel = new LoginViewModel
+            {
+
+            };
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public virtual ActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // usually this will be injected via DI. but creating this manually now for brevity
+            //if (authenticationResult.IsSuccess)
+            //{
+            //}
+
+            //ModelState.AddModelError("", authenticationResult.ErrorMessage);
+            return View(model);
+        }
+    }
+}
---
+++ Transportation/Transportation/Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Transportation.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username or Password is not correct."), AllowHtml]
+        [DisplayName("Student Email (or Faculty/Staff Username):")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = " "), AllowHtml]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string Token { get; internal set; }
+        public bool Success { get; internal set; }
+    }
+}
