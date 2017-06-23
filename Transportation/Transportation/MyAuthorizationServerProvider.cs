ba4ed1671ccbf63c57c6818ab4fbe8dc95e0cb17
JumperGerald <JumperGerald@users.noreply.github.com>
2017-06-23 10:27:49 -0700

    Added Required Namespaces
    If it doesn't compile comment out startup.cs

---
+++ Transportation/Transportation/MyAuthorizationServerProvider.cs
@@ -0,0 +1,48 @@
+﻿using Identity.Services.ActiveDirectory;
+using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CosAuthenticationApi
+{
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated(); //
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            string errorMessage = string.Empty;
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            if (!string.IsNullOrEmpty(context.UserName) && !string.IsNullOrEmpty(context.Password))
+            {
+                //Get the identity for the given user
+                identity = AuthService.GetIdentity(context, ref errorMessage);
+
+                if (identity != null && string.IsNullOrEmpty(errorMessage))
+                {
+                    //Valid identity found
+                    context.Validated(identity);
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(errorMessage))
+                    {
+                        context.SetError("invalid_grant", errorMessage);
+                    }
+                    else
+                    {
+                        context.SetError("invalid_grant", "Provided username and password is incorrect");
+                    }
+                }
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Please provide a valid username and password");
+            }
+        }
+    }
+}
