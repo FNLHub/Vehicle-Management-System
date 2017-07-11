f19c9dc9c118bb66bd9194784152787b1685dabc
Cory Lewis <cjl9703@yahoo.com>
2017-07-11 15:59:08 -0700

    Added: Admin Panel, and Add Gas Card Views

--- Voyager2.0/Voyager2.0/Controllers/AdminController.cs
+++ Voyager2.0/Voyager2.0/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TransportationDB;
 using Voyager2._0.Helpers;
 
 namespace Voyager2._0.Controllers
@@ -10,10 +11,52 @@ namespace Voyager2._0.Controllers
     [CosAuth]
     public class AdminController : Controller
     {
+        FacilitiesDBEntities transportationContext = new FacilitiesDBEntities();
+
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
+        
+        //----------------------------------------------- Create DB Entry Views  ----------------------------------------------------------
+        [HttpGet]
+        public ActionResult CreateGasCard()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateGasCard(Admin_Create_GasCard CGasCard)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var statusId = transportationContext.GasCardStatuses.Where(Gs => Gs.StatusName == CGasCard.StatusName).Select(i => i.StatusId).FirstOrDefault();
+                    if (statusId != 0)
+                    {
+                        //instantiate object
+                        GasCard gasCard = new GasCard();
+                        //set
+                        gasCard.GasCardName = CGasCard.GasCardName;
+                        gasCard.GasCardNum = CGasCard.GasCardNum;
+                        gasCard.GasCardPin = CGasCard.GasCardPin;
+                        gasCard.StatusId = statusId;
+                        //add
+                        transportationContext.GasCards.Add(gasCard);
+                        //save
+                        transportationContext.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    return new HttpStatusCodeResult(Response.StatusCode = 400);
+                }
+                catch
+                {
+                    return new HttpStatusCodeResult(Response.StatusCode = 400);
+                }
+            }
+            return View();
+        }
+        //----------------------------------------------- Create DB Entry Views End ----------------------------------------------------------
     }
 }
