3d235970c5a48762179c8849147c33101123bcac
Cory Lewis <cjl9703@yahoo.com>
2017-07-14 20:38:41 -0700

    Added Transportation Request View
    Additionally Added:
    Site.css data
    NewRequest.js file
    And created Viewbag.js & Viewbag.css which will include the necessary
    files for proper use.
    Note{
    Under Home Controller, NewRequest Rules added which will auto add user
    data once the api information is updated
    }

--- Voyager2.0/Voyager2.0/Controllers/HomeController.cs
+++ Voyager2.0/Voyager2.0/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace Voyager2._0.Controllers
     public class HomeController : Controller
     {
         FacilitiesDBEntities TransportationContext = new FacilitiesDBEntities();
+
         public ActionResult Index()
         {
             if (1==3) // if user is not authenticated
@@ -36,8 +37,39 @@ namespace Voyager2._0.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult NewRequest()
         {
+            //Currently Broken due to new Api token***********************************
+
+            ////Empty User
+            //TransporationRequest userFill = new TransporationRequest() { User = new Transportation.User() };
+
+            ////Fill Empty User from db
+            //userFill.User.FirstName = LoginController.GetAuthorize(Request.Cookies[LoginController.userToken]).userInfo.FirstName;
+            //userFill.User.LastName = LoginController.GetAuthorize(Request.Cookies[LoginController.userToken]).userInfo.LastName;
+            //userFill.User.BannerId = LoginController.GetAuthorize(Request.Cookies[LoginController.userToken]).userInfo.EmployeeId.Substring(1);
+            //userFill.User.OfficePhoneNumber = LoginController.GetAuthorize(Request.Cookies[LoginController.userToken]).userInfo.OfficePhone;
+            //userFill.User.Email = LoginController.GetAuthorize(Request.Cookies[LoginController.userToken]).userInfo.Email;
+            //return View(userFill);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewRequest(TransporationRequest transRequest)
+        {
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                }
+                catch
+                {
+                    return new HttpStatusCodeResult(Response.StatusCode = 400);
+                }
+            }
             return View();
         }
     }
