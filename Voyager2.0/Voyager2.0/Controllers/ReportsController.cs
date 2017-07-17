711e20c238b79531534be8335c02c1c78d647380
Cory Lewis <cjl9703@yahoo.com>
2017-07-17 14:39:38 -0700

    Added partial views for Reports & AddDelete Records

---
+++ Voyager2.0/Voyager2.0/Controllers/ReportsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Voyager2._0.Controllers
+{
+    public class ReportsController : Controller
+    {
+
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
