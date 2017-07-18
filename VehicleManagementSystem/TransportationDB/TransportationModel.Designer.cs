63f3f620a37a12043681fece62ed7cc8c0f92ea6
Cory Lewis <cjl9703@yahoo.com>
2017-07-18 16:42:13 -0700

    Reports Views & Controller Added..
    Created 2 new css files to prevent css from causing strange affects
    throughout the app where we don't want it.

--- VehicleManagementSystem/TransportationDB/TransportationModel.Designer.cs
+++ VehicleManagementSystem/TransportationDB/TransportationModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Gerald\Documents\Visual Studio 2017\Projects\VehicleManagementSystem\TransportationDB\TransportationModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Cory\Documents\GitHub\FacilitiesCheckout\VehicleManagementSystem\TransportationDB\TransportationModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
