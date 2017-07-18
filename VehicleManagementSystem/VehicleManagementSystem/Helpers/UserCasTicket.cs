54d2f88df38aa0f9d302684947c00ee2f3244282
Cory Lewis <cjl9703@yahoo.com>
2017-07-18 14:05:36 -0700

    Admin View and Create Gas Card Views added. Currently Working on making CAS work properly.
    probably a broken commit. no worries.  My next commit should have the
    DotNetCasClient.dll and .xml files inside and working..  wish me luck...

---
+++ VehicleManagementSystem/VehicleManagementSystem/Helpers/UserCasTicket.cs
@@ -0,0 +1,26 @@
+﻿using VehicleManagementSystem.Models;
+using System.Web;
+
+namespace VehicleManagementSystem.Helpers
+{
+    public class UserCasTicket
+    {
+        /// <summary>
+        /// Get the CAS ticket from HttpCookie
+        /// </summary>
+        /// <param name="ticketCookie"></param>
+        /// <returns></returns>
+        public CasTicketData GetTicket(HttpCookie ticketCookie)
+        {
+            CasTicketData casTicketClass = null;
+
+            if (ticketCookie != null)
+            {
+                CASTicketClass casTicket = new CASTicketClass();
+                casTicketClass = casTicket.GetTicketData(ticketCookie);
+            }
+
+            return casTicketClass;
+        }
+    }
+}
---
+++ VehicleManagementSystem/VehicleManagementSystem/Scripts/CustomScripts/newRequest.js
@@ -0,0 +1,52 @@
+﻿//New Request Javascript
+
+$(document).ready(function () {
+
+    //Array of div id's
+    let divStepId = [];
+
+    //Hide Submit Button
+    let submitButton = $("#btnSubmit");
+    submitButton.hide();
+
+    for (let i = 0; i < 6; i++) {
+
+        //Populate Array
+        divStepId[i] = $("#step" + (i + 1));
+        //Hide all step Divs
+        if (i > 0) {
+            divStepId[i].hide();
+
+            //Previous Button Pressed
+            divStepId[i].find("#btnStep" + (i + 1) + "Prev").click(function () {
+
+                divStepId[i - 1].slideDown(300);
+                //location.hash = "step2";
+                $("html,body").animate({
+                    scrollTop: divStepId[i - 1].offset().top
+                }, 1000);
+
+                divStepId[i].slideUp(300);
+
+                //show current next step and prev step
+                $("#btnStepGroup" + (i)).show();
+            });
+        }
+        if (i < 5) {
+
+            //Next Button Pressed
+            divStepId[i].find("#btnStep" + (i + 1) + "Next").click(function () {
+
+                divStepId[i + 1].slideDown(300);
+                //location.hash = "step2";
+                $("html,body").animate({
+                    scrollTop: divStepId[i + 1].offset().top
+                }, 1000);
+
+                //hide current next step and prev step
+                $("#btnStepGroup" + (i + 1)).hide();
+
+            });
+        }
+    };
+});
